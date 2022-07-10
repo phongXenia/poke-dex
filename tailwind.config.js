b18c530c3c4803280f7d8649129d67241a5a93c6
Phong Nguyen <phong.nguyen@xenia.tech>
2022-07-10 18:05:38 +0700

    Setup neccessary packages

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "bg-color": "#16171f",
+        "black": "#252A41",
+      }
+    },
+  },
+  plugins: [],
+}
